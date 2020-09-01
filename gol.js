a061e5a49ecb39167fc8d12fb283f3720df1e169
nullFoo <38643612+nullFoo@users.noreply.github.com>
2020-09-02 10:56:10 +1200

    Made the grid 15x15 because 10x10 was too small
    Later I'm gonna make it so we can put in a value and it can initialize a grid of any size

--- gol.js
+++ gol.js
@@ -1,15 +1,20 @@
-var line1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var line2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var line3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var line4 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var line5 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var line6 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var line7 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var line8 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var line9 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var line10 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line4 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line5 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0];
+var line6 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line7 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line8 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line9 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line10 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line11 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line12 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line13 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line14 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var line15 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-var grid = [line1, line2, line3, line4, line5, line6, line7, line8, line9, line10];
+var grid = [line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12, line13, line14, line15];
 
 var startR = 2;
 var AmountPerFrame = 4;
@@ -20,8 +25,8 @@ window.onload = function () {
  canvasArea = canvasObj.getContext("2d");
 
 
- for (var x = 0; x < 10; x++) {
-   for (var y = 0; y < 10; y++) {
+ for (var x = 0; x < 15; x++) {
+   for (var y = 0; y < 15; y++) {
      grid[x][y] = Math.floor(Math.random() * Math.floor(4));
    }
  }
@@ -40,8 +45,10 @@ function rgb(r,g,b) {
 }
 
 function getSurroundNumber(x, y) {
-    if(x == 0 || y == 0 || x == 9 || y == 9)
+    //TODO: Fix this function for edge squares, for now its just setting to 0
+    if(x == 0 || y == 0 || x == 14 || y == 14)
       return 0;
+
     //This is probably not the way to do it but I don't know a better way
     var amount = 0;
     if(grid[x+1][y] == 0)
@@ -68,21 +75,18 @@ function getSurroundNumber(x, y) {
 function update() {
 canvasArea.clearRect(0, 0, canvasObj.width, canvasObj.height);
 
-for (var x = 0; x < 10; x++) {
-  for (var y = 0; y < 10; y++) {
+for (var x = 0; x < 15; x++) {
+  for (var y = 0; y < 15; y++) {
     canvasArea.beginPath();
 
     if(grid[x][y] == 0)
       canvasArea.fillStyle = "white";
     else
       canvasArea.fillStyle = "black"
-    //TODO: Set color based off grid[x][2]
 
-    canvasArea.fillRect(x*40,y*40,40,40);
+    canvasArea.fillRect(x*30,y*30,30,30);
 
-    console.log(getSurroundNumber(x,y))
-
-    if(getSurroundNumber(x, y) > 4) {
+    if(getSurroundNumber(x, y) > 3) {
         grid[x][y] = 1;
     }
     else if(getSurroundNumber(x, y) < 2) {
